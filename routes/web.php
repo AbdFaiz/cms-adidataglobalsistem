6b6d466286317b604336d31d75d63c74ec46a840
fz <abdurrahmanfaiz187@gmail.com>
2025-08-13 00:01:46 +0700

    route web to api

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,6 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-// api email untuk dovecot
-Route::post('/api-email', [EmailController::class, 'webhookFromDovecot']);
-
 // Error routes
 Route::view('/404', 'errors.404')->name('404');
 Route::view('/500', 'errors.500')->name('500');
